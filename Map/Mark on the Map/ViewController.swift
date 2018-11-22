cd7e67d4ca9e5b8820d9e7cc5272265aa4132603
walkinded <labunskij1997@gmail.com>
2018-11-22 14:12:17 +0300

    Создание приложения
    - создан скелет приложения.
    - добавлены окна: Регистрация, Авторизация.
    - добавлено окно Карта
    - добавлен в проект Яндекс карты

---
+++ Map/Mark on the Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mark on the Map
+//
+//  Created by Roman Lab on 20.11.2018.
+//  Copyright © 2018 Roman_Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
